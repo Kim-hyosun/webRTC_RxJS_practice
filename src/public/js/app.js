6cb10d5d7fd8f3c935197f6e396f117ea773c623
Kim-hyosun <jirehmuze@gmail.com>
2023-10-15 07:25:17 +0900

    add: 채팅, nickname입력기능

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,11 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
 
+const makeMassage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
 const socket = new WebSocket(`ws://${window.location.host}`); //서버로의 연결
 
 socket.addEventListener('open', () => {
@@ -21,7 +26,15 @@ socket.addEventListener('close', () => {
 const handleSubmit = (e) => {
   e.preventDefault();
   const input = messageForm.querySelector('input');
-  socket.send(input.value); //내가input에써서 submit한 메시지 서버로 보냄
+  socket.send(makeMassage('new_message', input.value)); //내가input에써서 submit한 메시지 서버로 보냄
+  input.value = '';
+};
+
+const handleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMassage('nickname', input.value));
   input.value = '';
 };
 messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -21,10 +21,20 @@ const sockets = []; //사용자 연결위해
 wss.on('connection', (socket) => {
   //@socket : 브라우저로의 연결
   sockets.push(socket);
+  socket['nickname'] = 'Anonymous';
   console.log('connected to browser');
   socket.on('close', () => console.log('브라우저와 연결이 끊어졌습니다.'));
   socket.on('message', (msg) => {
-    sockets.forEach((aSocket) => aSocket.send(msg.toString())); //프론트에서 서버로 받은 메시지 바로 프론트로 보냄
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
+    //프론트에서 서버로 받은 메시지 바로 프론트로 보냄
   });
 });
 
